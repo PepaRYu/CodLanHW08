54fd028ef4a9208b136b8062d2e875c93b074f6b
DrPRun <sheighd@gmail.com>
2022-08-19 13:28:50 +0300

    Tret'ya zadacha reshena

---
+++ Zadacha056/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Задайте количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] GetArray (int m, int n , int minRandom, int maxRandom ){
+    int [,] result = new int[m, n];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j]  = new Random().Next(minRandom, maxRandom+ 1);
+        }
+        
+    }
+    return result;
+}
+void PrintArray(int [,] matrix){
+        for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void MinStringSumm(int [, ] array ){
+    int minSumm = int.MaxValue;
+    int stringCount = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sumStrinigElm = 0;        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumStrinigElm += array[i, j];
+        }
+        if ( minSumm > sumStrinigElm )
+        {
+            minSumm = sumStrinigElm;
+            stringCount = i;
+        }
+    }
+    Console.WriteLine($"Номер строки с минимальной суммой элементов {stringCount}, сумма равна {minSumm} ");
+}
+
+int [, ] rectangleMatrix = GetArray(rows, columns, 0, 5);
+PrintArray(rectangleMatrix);
+Console.WriteLine();
+MinStringSumm(rectangleMatrix);
---
+++ Zadacja058/Program.cs
@@ -0,0 +1,63 @@
+﻿Console.WriteLine("Задайте количество строк матрицы M");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов матрицы M и строк матрицы N");
+int RowsColums = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Задайте количество столбцов матрицы N");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+
+int [,] GetArray (int m, int n , int minRandom, int maxRandom ){
+    int [,] result = new int[m, n];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j]  = new Random().Next(minRandom, maxRandom+ 1);
+        }
+        
+    }
+    return result;
+}
+
+void PrintArray(int [,] matrix){
+        for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int [, ] MultiPlication(int [,] leftMatrix, int [, ] rightMatrix){
+    int [, ] multiplication = new int [leftMatrix.GetLength(0), rightMatrix.GetLength(1)];
+        for (int i = 0; i < leftMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < leftMatrix.GetLength(1); j++)
+            {
+                int currentElement = 0;
+                for (int k = 0; k < rightMatrix.GetLength(1); k++)
+                {
+                    currentElement+= leftMatrix[i, k]*rightMatrix[k, j];
+                }
+                multiplication[i,j] = currentElement;
+            }
+        }
+        return multiplication;
+}
+
+
+int [, ] MatrixM = GetArray(rows, RowsColums, 0, 10);
+int [, ] MatrixN = GetArray(RowsColums, columns, 0, 10);
+
+Console.WriteLine("Выводим первую матрицу на экран");
+PrintArray(MatrixM);
+Console.WriteLine("Выводим вторую матрицу на экран");
+PrintArray(MatrixN);
+Console.WriteLine("Выводим произведение матриц на экран");
+PrintArray(MultiPlication(MatrixM, MatrixN));
